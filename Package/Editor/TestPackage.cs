4100f189a8142fee23b17affabc06b0f171656b7
Salman Fallatah <115993283+Dontaks@users.noreply.github.com>
2022-10-26 11:26:18 +0300

    Package files

---
+++ Package/Editor/TestPackage.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
